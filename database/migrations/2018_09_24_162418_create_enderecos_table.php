ff63cc22bf82689e3f92e7a517392cdbdfad2ef4
dudu2599 <eduardo.2599@hotmail.com>
2018-09-25 08:44:02 -0300

    Primeiro commit.

---
+++ database/migrations/2018_09_24_162418_create_enderecos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('municipio');
+            $table->string('uf');
+            $table->string('pais');
+            $table->string('local');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('habitat');
+            $table->string('observacao');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
---
+++ database/migrations/2018_09_24_162517_create_exsicatas_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExsicatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exsicatas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('autor');
+            $table->string('escavaninho');
+            $table->string('coletor');
+            $table->date('data');
+            $table->string('determinador');
+            $table->integer('quantidade');
+            $table->string('bdd');
+            $table->text('image')->nullable();
+            $table->unsignedInteger('endereco_id');
+            $table->foreign('endereco_id')->references('id')->on('enderecos')->onDelete('cascade');
+            $table->unsignedInteger('genero_id');
+            $table->foreign('genero_id')->references('id')->on('generos')->onDelete('cascade');
+            $table->unsignedInteger('epiteto_id');
+            $table->foreign('epiteto_id')->references('id')->on('epitetos')->onDelete('cascade');
+            $table->unsignedInteger('familia_id');
+            $table->foreign('familia_id')->references('id')->on('familias')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exsicatas');
+    }
+}
